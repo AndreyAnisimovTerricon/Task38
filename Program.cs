2bf11da0a57211816ebaf33b43b7e56961951d81
AndreyAnisimovTerricon <atriummebel79@yandex.ru>
2022-09-05 20:25:09 +0300

    разница между макс. и мин. элементом массива

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿int size = 5;
+double [ ] array = new double [size];
+for(int i =0; i <array.Length; i ++)
+{                                           
+ array[ i] = new Random().Next(0,100);
+ }
+
+Console.WriteLine(string.Join(",", array));
+double maxValue = array.Max<double>();
+double minValue = array.Min<double>();
+ double result = maxValue - minValue; 
+ Console.Write("Разность равна : ");
+Console.WriteLine(result);
